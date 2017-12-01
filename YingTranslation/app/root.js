5df4ab4d77c6a645d265f941f12f6ee54926ad9d
KADFWJ <930135305@qq.com>
2017-12-01 17:49:41 +0800

    完成code-push安卓与ios的配置

--- YingTranslation/app/root.js
+++ YingTranslation/app/root.js
@@ -6,13 +6,69 @@
 
 import App from "./containers/app";
 import React, { Component } from "react";
+import { Alert } from "react-native";
 import { Provider } from 'react-redux';
 import store from './store/store';
+import CodePush from 'react-native-code-push';
 
-const Root = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
+
+//android-staging r9g2x1Fb_1sx8tmM7KSRa3AA7cja3e570e6d-1756-4fea-ac2a-83ad60454c8e
+//ios-staging   1lX9t67WozG8DjDE0Az1QzZa7FpL3e570e6d-1756-4fea-ac2a-83ad60454c8e
+const deploymentKey = "r9g2x1Fb_1sx8tmM7KSRa3AA7cja3e570e6d-1756-4fea-ac2a-83ad60454c8e";
+// const Root = () => (
+    // <Provider store={store}>
+    //     <App />
+    // </Provider>
+// );
+class Root extends Component {
+
+    componentDidMount(){
+        CodePush.checkForUpdate(deploymentKey).then((update) => {
+            if (!update) {
+                Alert.alert("提示", "已是最新版本--", [
+                    {
+                        text: "Ok", onPress: () => {
+                            console.log("点了OK");
+                        }
+                    }
+                ]);
+            } else {
+                codePush.sync({
+                    deploymentKey: deploymentKey,
+                    updateDialog: {
+                        optionalIgnoreButtonLabel: '稍后',
+                        optionalInstallButtonLabel: '立即更新',
+                        optionalUpdateMessage: '有新版本了，是否更新？',
+                        title: '更新提示'
+                    },
+                    installMode: CodePush.InstallMode.IMMEDIATE,
+
+                },
+                    (status) => {
+                        switch (status) {
+                            case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+                                console.log("DOWNLOADING_PACKAGE");
+                                break;
+                            case CodePush.SyncStatus.INSTALLING_UPDATE:
+                                console.log(" INSTALLING_UPDATE");
+                                break;
+                        }
+                    },
+                    (progress) => {
+                        console.log(progress.receivedBytes + " of " + progress.totalBytes + " received.");
+                    }
+                );
+            }
+        })
+    }
+
+    render(){
+        return(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        )
+    }
+}
 
 export default Root;
