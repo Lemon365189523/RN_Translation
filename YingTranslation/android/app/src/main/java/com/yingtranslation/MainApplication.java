e1d30e5885c7024d33e71b2403109b9a998b4866
KADFWJ <930135305@qq.com>
2017-12-01 16:02:25 +0800

    之前配置错误，现在配置好了，安卓可以运行项目

--- YingTranslation/android/app/src/main/java/com/yingtranslation/MainApplication.java
+++ YingTranslation/android/app/src/main/java/com/yingtranslation/MainApplication.java
@@ -12,7 +12,6 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
           new VectorIconsPackage()
       );
     }
