f2a8fb62879bea9892f475c001b00765e85ae2de
KADFWJ <930135305@qq.com>
2018-01-04 16:51:04 +0800

    iOS配置分享环境完成

--- YingTranslation/app/pages/words/WordDetailsPage.js
+++ YingTranslation/app/pages/words/WordDetailsPage.js
@@ -14,6 +14,9 @@ import Imgs from '../../img';
 import YTSpringView from '../../components/YTSpringView';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 const { Surface, Shape, Path, Group} = ART;
+import SharePlatform from '../../librarys/share/SharePlatform';
+import UShare from '../../librarys/share/share'
+
 
 const art_height = 90;
 const radius = 300;
@@ -53,7 +56,22 @@ export default class WordDetailsPage extends React.Component{
     componentWillMount(){
         this.props.navigation.setParams({
             onClickShareBtn:()=>{
-                console.log('点击分享');
+
+                /** 
+       * 参数说明： 
+       * 1. 标题 
+       * 2. 内容 
+       * 3. 跳转链接 
+       * 4. 图片链接 
+       * 5. 分享平台 
+       * 6. 分享结果回调 
+       */
+                UShare.share('标题', '内容', 'http://baidu.com', 'http://dev.umeng.com/images/tab2_1.png', SharePlatform.QQ, (message) => {
+                    // message:分享成功、分享失败、取消分享
+                    // ToastAndroid.show(message,ToastAndroid.SHORT);  
+                    console.log(message);
+                });  
+
             }
         })
     }
