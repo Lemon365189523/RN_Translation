72bf24d68740f2ad100b841c79c2014718f2e041
KADFWJ <930135305@qq.com>
2018-01-19 17:55:41 +0800

    添加安卓的分享库完成，不上传libs和jniLibs里的第三方库 到时下载拖回去

--- YingTranslation/android/app/src/main/java/com/yingtranslation/MainActivity.java
+++ YingTranslation/android/app/src/main/java/com/yingtranslation/MainActivity.java
@@ -3,8 +3,11 @@ package com.yingtranslation;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;
 
 import org.devio.rn.splashscreen.SplashScreen;
+import com.yingtranslation.module.ShareModule;
+import com.umeng.socialize.UMShareAPI;
 
 public class MainActivity extends ReactActivity {
 
@@ -25,6 +28,21 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this );  // here
         super.onCreate(savedInstanceState);
+        ShareModule.initActivity(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 解决内存泄漏问题
+        UMShareAPI.get(this).release();
     }
 
 }
---
+++ YingTranslation/android/app/src/main/java/com/yingtranslation/apshare/ShareEntryActivity.java
@@ -0,0 +1,7 @@
+package com.yingtranslation.apshare;
+
+import com.umeng.socialize.media.ShareCallbackActivity;
+
+public class ShareEntryActivity extends ShareCallbackActivity {
+
+}
